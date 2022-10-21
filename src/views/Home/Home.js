6dec1cb00fdd681b3bac9d5b26b214a579330e00
Melissa Göşmen <47207804+melissagosmen@users.noreply.github.com>
2022-10-22 00:34:00 +0300

    Kurulumlar yapıldı

---
+++ src/views/Home/Home.js
@@ -0,0 +1,21 @@
+import './home.scss'
+import React, { useState, useEffect } from 'react';
+
+
+function Home() {
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+        document.title = `You clicked ${count} times`;
+    });
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+        </div>
+    )
+}
+
+export default Home
